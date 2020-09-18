e93b6c0cfc1816f5362ce44a2029c8bc4d4dad4c
JrLalamoro <lalamorojr@gmail.com>
2020-09-18 17:41:10 +0800

    setup initial database

---
+++ database/migrations/2020_09_18_091644_create_table_jobs.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableJobs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('job_title');
+            $table->text('job_description')->nullable();
+            $table->date('date')->nullable();
+            $table->integer('location_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
